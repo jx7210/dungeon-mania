a79e73c99a39214b33d54b7e216f9e5bf5afc80f
Karen Lue <karenlue@192-168-1-100.tpgi.com.au>
2023-04-08 01:12:17 +1000

    progress

---
+++ src/main/java/dungeonmania/goals/Microevolution.java
@@ -0,0 +1,21 @@
+package dungeonmania.goals;
+
+import dungeonmania.Game;
+
+public class Microevolution implements Goal {
+    public Microevolution() {
+    }
+
+    @Override
+    public boolean achieved(Game game) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'achieved'");
+    }
+
+    @Override
+    public String toString(Game game) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+
+}
