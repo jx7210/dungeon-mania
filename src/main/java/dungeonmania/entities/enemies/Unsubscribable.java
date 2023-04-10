2d6a9beeb1fd857ef2a6618a707f91ce2e983dc7
Karen Lue <karenlue@192-168-1-100.tpgi.com.au>
2023-04-11 00:12:11 +1000

    merge conflicts fixed

--- src/main/java/dungeonmania/entities/enemies/Unsubscribable.java
+++ src/main/java/dungeonmania/entities/enemies/Unsubscribable.java
@@ -1,21 +1,7 @@
 package dungeonmania.entities.enemies;
 
-import dungeonmania.Game;
-import dungeonmania.entities.Entity;
-// import dungeonmania.entities.Player;
 import dungeonmania.map.GameMap;
-import dungeonmania.util.Position;
 
-public abstract class Unsubscribable extends Entity {
-    public Unsubscribable(Position position) {
-        super(position);
-    }
-
-    public void onDestroy(GameMap map) {
-        Game g = map.getGame();
-        g.unsubscribe(getId());
-
-        // Player player = map.getPlayer();
-        // player.defeatEnemy();
-    }
+public interface Unsubscribable {
+    public void onDestroy(GameMap map);
 }
