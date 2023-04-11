676e5670b275fc2b39fdd6d4329fc93d8190d57a
Amanda Lu <acaplaamall@DESKTOP-LBQIULP.localdomain>
2023-04-11 16:43:26 +1000

    dryrun

--- src/main/java/dungeonmania/entities/Effectible.java
+++
@@ -1,7 +0,0 @@
-package dungeonmania.entities;
-
-import dungeonmania.map.GameMap;
-
-public interface Effectible {
-    public void onOverlap(GameMap map, Entity entity);
-}
--- src/main/java/dungeonmania/entities/enemies/Unsubscribable.java
+++
@@ -1,7 +0,0 @@
-package dungeonmania.entities.enemies;
-
-import dungeonmania.map.GameMap;
-
-public interface Unsubscribable {
-    public void onDestroy(GameMap map);
-}
